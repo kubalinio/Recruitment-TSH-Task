17c3803724d19066c0fdb3f28e3b0c1cf5168e55
Jakub <jsapalawebdev@gmail.com>
2023-11-10 19:43:12 +0100

    feat: setup header layout for desktop & improve link for login page

--- src/app/shared/layouts/DefaultLayout/components/SearchBar.tsx
+++ src/app/shared/layouts/DefaultLayout/components/SearchBar.tsx
@@ -3,14 +3,13 @@ import { SearchInput } from 'app/shared/components/SearchInput'
 
 export function SearchBar() {
     return (
-        <div className='order-3 w-full mt-7'>
-            <SearchInput />
+        <div className='order-3 w-full mt-7 md:order-2 md:w-2/4 md:flex md:items-center md:mt-0 md:gap-x-6'>
+            <SearchInput className='md:flex-grow' />
 
-            <div className='flex mt-6 gap-x-8'>
+            <div className='flex mt-6 gap-x-8 md:mt-0'>
                 <CheckboxWithText id="active" label="Active" />
                 <CheckboxWithText id="promo" label="Promo" />
             </div>
-
         </div>
     )
 }
--- src/app/shared/layouts/DefaultLayout/containers/Header.tsx
+++ src/app/shared/layouts/DefaultLayout/containers/Header.tsx
@@ -1,21 +1,20 @@
-import { Button } from "app/shared/ui";
+import { Link } from "react-router-dom";
+
+import { buttonVariants } from "app/shared/ui";
 import { SearchBar } from "../components/SearchBar";
 
 export function Header() {
     return (
-        <header className="flex flex-wrap items-center justify-between w-full px-6 bg-white pt-14 pb-9">
+        <header className="flex flex-wrap items-center justify-start w-full px-6 bg-white lg:mx-auto pt-14 pb-9 md:py-14 lg:max-w-7xl">
+            <Link to={'/'}>
+                <h1 className="w-1/2 text-2xl font-semibold md:w-auto md:mr-[105px]">join.tsh.io</h1>
+            </Link>
 
-            <h1 className="w-1/2 text-2xl font-semibold">join.tsh.io</h1>
-
-            <Button className="w-[88px]" variant={'outline'}>
-                <a href="/login" >
-                    Log in
-                </a>
-            </Button>
+            <Link to={'/login'} className={buttonVariants({ variant: "outline", className: "max-w-[88px] md:w-1/4 md:order-3 ml-auto flex-none p-0" })}>
+                Log in
+            </Link>
 
             <SearchBar />
-
-
         </header>
     )
 }
